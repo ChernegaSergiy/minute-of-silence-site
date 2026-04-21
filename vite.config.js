699be1cce901675acd8a644e8ad5dd531367875b
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-04-21 11:47:57 +0000

    build: Configure vite-plugin-image-optimizer

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    ViteImageOptimizer({
+      png: { quality: 80 },
+      jpeg: { quality: 80 },
+      webp: { quality: 80 }
+    })
+  ],
   server: {
     port: 5173,
     host: true
