3072512035e9af5e5fedb8fdfcc3c0658d43a370
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-04-21 12:51:09 +0000

    feat: Add i18n to Footer component

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,13 +1,14 @@
 <script>
+  import { t } from '../i18n/index.js';
 </script>
 
 <footer class="footer">
   <div class="footer-row">
-    <div>Хвилина мовчання · <a class="underline hover:underline-wavy" href="https://github.com/ChernegaSergiy/minute-of-silence" target="_blank">GitHub</a> · <a class="underline hover:underline-wavy" href="https://github.com/ChernegaSergiy/minute-of-silence/releases" target="_blank">Releases</a></div>
+    <div>{$t.hero.title} · <a class="underline hover:underline-wavy" href="https://github.com/ChernegaSergiy/minute-of-silence" target="_blank">GitHub</a> · <a class="underline hover:underline-wavy" href="https://github.com/ChernegaSergiy/minute-of-silence/releases" target="_blank">Releases</a></div>
     <div>© 2026</div>
   </div>
   <div class="footer-row opacity-70" style="border-top: 1px solid #000; padding-top: 0.5rem;">
-    <div>Мінімалістичний бруталізм · Чорне / біле · Без зайвих ефектів</div>
-    <div>Зроблено в Україні 🇺🇦</div>
+    <div>{$t.footer.style}</div>
+    <div>{$t.footer.madeIn}</div>
   </div>
 </footer>
