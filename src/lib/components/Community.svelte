5fd6beb37f0414a5f4a894a6969ea7fc8481c899
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-09-17 01:44:38 +0300

    style: Change button to white in Community section for better contrast

--- src/lib/components/Community.svelte
+++ src/lib/components/Community.svelte
@@ -21,7 +21,7 @@
       
       <div class="flex flex-col sm:flex-row gap-4 items-start sm:items-center justify-between">
         <a href="https://www.instagram.com/vshanuy/"
-           class="btn btn--primary"
+           class="btn bg-white text-black border-white hover:bg-gray-200 hover:border-gray-200"
            target="_blank"
            rel="noopener noreferrer">
           {$t.community.button}
