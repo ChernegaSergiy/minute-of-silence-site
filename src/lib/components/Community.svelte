77627ed50fcf8ac9f159ff2516c3f0b9a9eae999
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-09-17 11:28:25 +0300

    fix: Update Instagram handle to moment_of_honor

--- src/lib/components/Community.svelte
+++ src/lib/components/Community.svelte
@@ -20,7 +20,7 @@
       </p>
       
       <div class="flex flex-col sm:flex-row gap-4 items-start sm:items-center justify-between">
-        <a href="https://www.instagram.com/vshanuy/"
+        <a href="https://www.instagram.com/moment_of_honor/"
            class="btn btn-inverted text-black underline hover:underline-wavy"
            target="_blank"
            rel="noopener noreferrer">
